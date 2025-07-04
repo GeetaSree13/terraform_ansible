1cc46e75de05f0ae2fd99db77dbd2e1e907e95b0
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-07-04 11:56:06 +0530

    added

--- scripts/dynamic_inventory.sh
+++ scripts/dynamic_inventory.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-
 chmod 600 ../ansible/id_rsa
-
-cd terraform || exit 1  # move into terraform dir or exit if not found
+cd terraform
 external_ip=$(terraform output -raw vm_ip)
-
 cd ..
-
-if [ -z "$external_ip" ]; then
-  echo "❌ ERROR: Failed to fetch external IP from Terraform output"
-  exit 1
-fi
-
 file="../ansible/inventory.yml"
 
 cat <<EOF > $file
@@ -21,4 +12,4 @@ all:
       ansible_host: $external_ip
       ansible_user: rocky
       ansible_ssh_private_key_file: ../ansible/id_rsa
-EOF
+EOF    
