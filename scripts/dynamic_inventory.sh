0b78d12614c7fa10bfd1ea99fe6dd3472222b2e9
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-07-04 12:12:36 +0530

    added

--- scripts/dynamic_inventory.sh
+++ scripts/dynamic_inventory.sh
@@ -3,12 +3,11 @@ chmod 600 ../ansible/id_rsa
 external_ip=$(terraform output -raw vm_ip)
 
 file="../ansible/inventory.yml"
-
-cat <<EOF > $file
+cat <<EOF > ../ansible/inventory.yml
 all:
-  hosts:
-    rocky-vm:
-      ansible_host: $external_ip
-      ansible_user: penumarthigeetasri
-      ansible_ssh_private_key_file: ../ansible/id_rsa
-EOF    
+    hosts:
+        rocky:
+            ansible_host: $external_ip
+            ansible_user: penumarthigeetasri
+            ansible_ssh_private_key_file: ../ansible/id_rsa
+EOF            
