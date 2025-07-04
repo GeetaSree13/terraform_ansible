07f2b31c75b032fe9d368836836c260fc5730948
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-07-04 10:54:01 +0530

    newly

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,6 @@
 variable "project_id"           { description = "GCP project ID" }
 variable "region"               { description = "GCP region" }
 variable "zone"                 { description = "GCP zone" }
-variable "google_credentials"     { description = "Path to service‑account JSON" }
 
 variable "instance_name"        { description = "VM name" }
 variable "machine_type"         { description = "Machine type" }
